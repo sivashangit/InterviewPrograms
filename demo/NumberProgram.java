4c7bd46c64e7c99750763605565b5db9395b0e46
sivakumar <sivaktvr@gmail.com>
2025-02-12 06:37:35 +0530

    new program added

---
+++ demo/NumberProgram.java
@@ -0,0 +1,15 @@
+package com.javatechie.lambda.demo.demo;
+
+public class NumberProgram {
+
+    static  void mth(){
+
+        System.out.println("welcome");
+    }
+
+    public static void main(String[] args)
+    {
+        mth();
+    }
+
+}
